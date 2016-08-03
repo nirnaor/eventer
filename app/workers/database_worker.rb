da6bc453415857f9b25d8655b68f828ae366824f
nirnaor <nirnaori@gmail.com>
2016-08-03 19:03:30 +0300

    Adding messages to queue on exceptions.

--- app/workers/database_worker.rb
+++ app/workers/database_worker.rb
@@ -2,12 +2,17 @@
 class DatabaseWorker
   @queue = :db
   def self.perform(emails)
-    # Pluck will load only the email field.
-    existing_emails = User.where(email: emails).pluck(:email).to_set
+    begin
+      # Pluck will load only the email field.
+      existing_emails = User.where(email: emails).pluck(:email).to_set
 
-    # Sets improve the difference calculation.
-    new_emails = emails.to_set - existing_emails
+      # Sets improve the difference calculation.
+      new_emails = emails.to_set - existing_emails
 
-    User.bulk_insert values: new_emails.map { |x| { email: x } }
+      User.bulk_insert values: new_emails.map { |x| { email: x } }
+    rescue ActiveRecord::ActiveRecordError => e
+      Resque.enqueue(DatabaseWorker, emails)
+      raise e
+    end
   end
 end
--- app/workers/mail_worker.rb
+++ app/workers/mail_worker.rb
@@ -2,7 +2,14 @@
 class MailWorker
   @queue = :mail
   def self.perform(emails, content)
-    existing_emails = User.where(email: emails).pluck(:email)
-    UserNotifierMailer.send_signup_email(existing_emails, content).deliver
+    begin
+      existing_emails = User.where(email: emails).pluck(:email)
+      UserNotifierMailer.send_signup_email(existing_emails, content).deliver
+    rescue StandardError => e
+      # I should probably seperate the exceptions here to ActiveRecordError
+      # and mail sending related error.
+      Resque.enqueue(MailWorker, emails, content)
+      raise e
+    end
   end
 end
