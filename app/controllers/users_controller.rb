8632c509a496655ad4973593692e5650fa562c14
nirnaor <nirnaori@gmail.com>
2016-08-03 14:29:30 +0300

    Sending emails with content through the controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,14 @@ class UsersController < ApplicationController
   def add
     Resque.enqueue(DatabaseWorker, JSON.parse(params[:emails]))
     render json: {users: 5}.to_json
-    # UserNotifierMailer.send_signup_email.deliver
   end
 
   def send_mail
+    emails, content = JSON.parse(params[:emails]), params[:content]
+    existing_emails = User.where(email: emails).pluck(:email)
+    existing_emails.each do |mail|
+      UserNotifierMailer.send_signup_email(mail, content).deliver
+    end
     render json: {users: 5}.to_json
   end
 end
