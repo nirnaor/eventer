7b959e009e5ddc3831b5ee0077ff7fff994cbadf
nirnaor <nirnaori@gmail.com>
2016-08-03 10:37:12 +0300

    Sending myself a mail using sendgrid.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def add
     render json: {users: 5}.to_json
+    UserNotifierMailer.send_signup_email.deliver
   end
 
   def send_mail
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
 require_relative 'application'
 
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USER'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => ENV['SENDGRID_DOMAIN'],
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Initialize the Rails application.
 Rails.application.initialize!
