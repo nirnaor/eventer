5a7086f3c54c747d4db7819faa20b8d8ac576113
nirnaor <nirnaori@gmail.com>
2016-08-03 11:28:36 +0300

    Added User model. Inserting only new emails.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
   def add
-    render json: {users: 5}.to_json
-    UserNotifierMailer.send_signup_email.deliver
+    emails = JSON.parse(params[:emails])
+    existing_emails = User.where(email: emails).pluck(:email).to_set
+    new_emails = emails.to_set - existing_emails
+    new_emails.each do |email|
+      User.create(email: email)
+    end
+    render json: {users: new_emails.size}.to_json
+    # UserNotifierMailer.send_signup_email.deliver
   end
 
   def send_mail
